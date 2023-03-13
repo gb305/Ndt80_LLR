99fe9b609c5bb749e29859f2089ebcf79891076c
Neale-Lab <47629455+Neale-Lab@users.noreply.github.com>
2023-03-13 15:45:42 +0000

    Update Hotspot_Smooth_ratios_V1.R

--- LUZ_SCRIPTs_forNDT80paper/Hotspot_Smooth_ratios_V1.R
+++ LUZ_SCRIPTs_forNDT80paper/Hotspot_Smooth_ratios_V1.R
@@ -2,12 +2,12 @@
 ##########################  HOTSPOT_ RATIOS  ########################
 ###########################################################################
 #Load chromosome number and length
-setwd("/Users/ll381/Dropbox/TESIS_DOCTORAL/R/RFILES")
+setwd("ENTER WORKING DIRECTORY")
 chromlengths <- read.table("yeast_chrom_sizes.txt")
 chromlengths$kb <- round(chromlengths[,2]/1000, digits=1)
 
 #Setwt to Hotspot.table files
-setwd("/Users/ll381/Dropbox/TESIS_DOCTORAL/R/RFILES/TEMPLATE_STUDY/CUTOOF_0.193/tel1D_control")
+setwd("ENTER WORKING DIRECTORY")
 parent.directory <- getwd()
 setwd(parent.directory); 
 
